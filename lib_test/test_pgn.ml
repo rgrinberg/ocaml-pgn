ca2928ed2e65e3681e62f1da0e5c0d8dc3757bc5
Rudi Grinberg <rudi.grinberg@gmail.com>
2012-10-27 00:26:51 -0400

    added some real unit tests

--- lib_test/test_pgn.ml
+++ lib_test/test_pgn.ml
@@ -1,5 +1,18 @@
-
-open Pgn
 open OUnit
 
-let () = print_endline "Testing pgn library"
+let pgn_root = "../samples"
+
+let g1 = List.hd (Pgn.parse_str "[one \"two\"]\n 1.e4 e5")
+
+let test_fixtures = 
+  "test pgn parser" >:::
+    [
+      "test mdata 1" >:: (fun () ->
+        assert_equal (Pgn.Mdata.get_exn g1 ~key:"one") "two";
+      );
+      "test moves 1" >:: (fun () ->
+        assert_equal (g1.Pgn.moves) ["e4";"e5"];
+      );
+    ]
+
+let _ = run_test_tt ~verbose:true test_fixtures
