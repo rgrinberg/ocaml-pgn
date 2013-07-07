150263b85d3f88e05110f6fbea9c4bbffc04b14a
Rudi Grinberg <rudi.grinberg@gmail.com>
2013-07-07 16:05:43 -0400

    refactored game mli to use more inner modules

--- lib/game.mli
+++ lib/game.mli
@@ -1,45 +1,50 @@
-
-type piece = 
-  | Pawn
-  | Knight
-  | Bishop
-  | Rook
-  | Queen
-  | King with sexp
-
-type color = Black | White with sexp
-
-type result = 
-  | Win of color 
-  | Draw with sexp
-
-type game_piece = {
-  piece : piece;
-  (* put this in common *)
-  color : color
-} with sexp
-
-type board (* abstract for now *)
-
-module Castling : sig
-  type t = K | Q | Both | Neither with sexp
-  val (+) : t -> t -> t
-  val (-) : t -> t -> t
+module Color : sig
+  type t = Black | White with sexp
 end
 
-type coord with sexp
+module Piece :
+sig
+  type t = Pawn | Knight | Bishop | Rook | Queen | King with sexp
+end
 
-type move with sexp
+module Board :
+sig
+  type t = (Piece.t option) array array with sexp
+  type coord = int * int with sexp
+  type move = 
+    | Remove of coord 
+    | Move of coord * coord with sexp
+  val make_move : t -> move -> unit
+  val create : dimx:int -> dimy:int -> t
+end
 
-type state with sexp
+module Algebraic : (module type of Algebraic)
 
-val create : board: board ->
+type result = Win of Color.t | Draw with sexp
+type game_piece = { piece : Piece.t; color : Color.t; } with sexp
+module Castling :
+sig
+  type t = K | Q | Both | Neither with sexp
+  val ( + ) : t -> t -> t
+  val ( - ) : t -> t -> t
+end
+type state = {
+  board : Board.t;
+  white_castled : Castling.t;
+  black_castled : Castling.t;
+  turn : Color.t;
+  en_passent : Board.coord option;
+  halfmove_clock : int;
+  fullmove_clock : int;
+  white_king : Board.coord;
+  black_king : Board.coord;
+}
+val find_kings : 'a -> 'b
+val create :
+  board:Board.t ->
   white_castled:Castling.t ->
   black_castled:Castling.t ->
-  turn:color ->
-  en_passent: coord option ->
-  halfmove_clock:int ->
-  fullmove_clock:int ->
-  state
-
-val evaluate : state -> [`Finished of result | `Ongoing]
+  turn:Color.t ->
+  en_passent:Board.coord option ->
+  halfmove_clock:int -> fullmove_clock:int -> state
+val evaluate : 'a -> 'b
