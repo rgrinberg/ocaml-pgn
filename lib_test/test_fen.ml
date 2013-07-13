77f319b2deb13b25d43634f71a768a9de730b706
Rudi Grinberg <rudi.grinberg@gmail.com>
2013-07-13 16:47:54 -0400

    removed ounit where it is not needed

--- lib_test/test_fen.ml
+++ lib_test/test_fen.ml
@@ -1,7 +1,8 @@
 open Core.Std
-open OUnit
 open Chess
 
+(* not a real test, we're just eyeballing whether the position looks like
+   it would fit*)
 let fen_start = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 
 let game = Chess.Fen.to_game fen_start
