92999f9a83d1d40930e20335d625a8b70180beb8
Rudi Grinberg <rudi.grinberg@gmail.com>
2013-07-13 16:28:48 -0400

    write fen

--- lib_test/test_fen.ml
+++ lib_test/test_fen.ml
@@ -1,6 +1,11 @@
+open Core.Std
 open OUnit
 open Chess
 
 let fen_start = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 
 let game = Chess.Fen.to_game fen_start
+
+let sexp = Game.sexp_of_state game
+
+let () = printf "Beginning position:\n%s\n" (Sexp.to_string_hum sexp)
