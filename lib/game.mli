db2af036b19856f2c2d4d56083865853bd0b6d8d
Rudi Grinberg <rudi.grinberg@gmail.com>
2013-07-13 16:06:29 -0400

    mli formatting

--- lib/game.mli
+++ lib/game.mli
@@ -2,13 +2,11 @@ module Color : sig
   type t = Black | White with sexp
 end
 
-module Piece :
-sig
+module Piece : sig
   type t = Pawn | Knight | Bishop | Rook | Queen | King with sexp
 end
 
-module Board :
-sig
+module Board : sig
   type 'a t = 'a option array array with sexp
   type coord = int * int with sexp
   type move = 
@@ -19,13 +17,15 @@ sig
 end
 
 type result = Win of Color.t | Draw with sexp
+
 type game_piece = { piece : Piece.t; color : Color.t; } with sexp
-module Castling :
-sig
+
+module Castling : sig
   type t = K | Q | Both | Neither with sexp
   val ( + ) : t -> t -> t
   val ( - ) : t -> t -> t
 end
+
 type state = {
   board : game_piece Board.t;
   white_castled : Castling.t;
@@ -37,7 +37,7 @@ type state = {
   white_king : Board.coord;
   black_king : Board.coord;
 }
-val find_kings : 'a -> 'b
+
 val create :
   board: game_piece Board.t ->
   white_castled:Castling.t ->
@@ -45,4 +45,5 @@ val create :
   turn:Color.t ->
   en_passent:Board.coord option ->
   halfmove_clock:int -> fullmove_clock:int -> state
+
 val evaluate : 'a -> 'b
