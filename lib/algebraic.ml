ece3b2de9c8fd42ac25db385b4aa56e8effb6e36
Rudi Grinberg <rudi.grinberg@gmail.com>
2013-07-13 13:31:39 -0400

    added proper algebraic coordinate conversion

--- lib/algebraic.ml
+++ lib/algebraic.ml
@@ -4,6 +4,27 @@ type rank = int with sexp
 
 type file = [`a | `b | `c | `d | `e | `f | `g | `h] with sexp
 
+let file_to_int = function
+  | `a -> 0
+  | `b -> 1
+  | `c -> 2
+  | `d -> 3
+  | `e -> 4
+  | `f -> 5
+  | `g -> 6
+  | `h -> 7
+
+let file_of_int = function
+  | 0 -> `a
+  | 1 -> `b
+  | 2 -> `c
+  | 3 -> `d
+  | 4 -> `e
+  | 5 -> `f
+  | 6 -> `g
+  | 7 -> `h
+  | _ -> invalid_arg "file_of_int"
+
 let file_of_char = function
   | 'a' -> `a
   | 'b' -> `b
@@ -15,6 +36,8 @@ let file_of_char = function
   | 'h' -> `h
   | x -> invalid_arg ("Not a file " ^ (String.of_char x))
 
+let rank_of_int x = 8 - x
+
 type algebraic_coord = file * rank with sexp
 
 let of_string s = 
@@ -25,7 +48,8 @@ let of_string s =
   with Invalid_argument _ -> (* in case out of bounds *)
     invalid_arg ("Bad algebraic coordinate: " ^ s)
 
-let of_board_coord coord = failwith "TODO"
+let of_board_coord (file, rank) = 
+  (file_of_int file, rank_of_int rank) (* 1,4 -> e7 *)
 
 let to_board_coord alg_coord = failwith "TODO"
 
@@ -39,6 +63,6 @@ module Move = struct
     | Pawn of file * rank
     | PawnCapture of file * file
     | Promotion of Piece.t * [`Initial of file] * [`Final of file]
-    | Move of Piece.t * rank * file
-    | FullMove of Piece.t * rank * file * rank * file with sexp
+    | Move of Piece.t * file * rank
+    | FullMove of Piece.t * file * rank * file * rank with sexp
 end
